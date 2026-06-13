800cdd65397f2ba0d7e7ec5eb293dc0b9e15d58e
leviet07052008-collab <leviet07052008@gmail.com>
2026-06-13 15:25:57 +0700

    Delete Aniban.mm

--- Aniban.mm
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-extern "C" {
-    void fake_antiban_signature() {
-        NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-        [defaults setObject:@"com.ff.original" forKey:@"game_signature"];
-        [defaults synchronize];
-    }
-}
